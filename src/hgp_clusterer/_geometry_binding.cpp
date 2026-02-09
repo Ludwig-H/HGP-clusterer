1c86a37c182bd0cb32ab5a37cbd42062a3a11c38
Ludwig-H <158177790+Ludwig-H@users.noreply.github.com>
2026-02-09 22:29:20 +0000

    Fix weight sign in Geogram backend for Order-K Delaunay

--- src/hgp_clusterer/_geometry_binding.cpp
+++ src/hgp_clusterer/_geometry_binding.cpp
@@ -239,7 +239,10 @@ py::tuple compute_delaunay(
 
                 // Radius^2 - Distance^2
                 // Order-k definition logic (Lifted weight)
-                bary_weights[i] = center_sq_norm - (sum_sq_norms * inv_k);
+                // We pass POSITIVE weights (R^2) to the backend.
+                // center_sq_norm - (sum_sq_norms * inv_k) is -Variance (Negative).
+                // So we flip: (sum_sq_norms * inv_k) - center_sq_norm.
+                bary_weights[i] = (sum_sq_norms * inv_k) - center_sq_norm;
         #ifdef CGAL_LINKED_WITH_TBB
             }
         });
